1a008055d1443e6166d8a7440f5baedd907ec9d9
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-08-03 13:07:54 +0300

    Update __init__.py

--- src/projects/__init__.py
+++ src/projects/__init__.py
@@ -1,5 +1,7 @@
 from metadata import register_meta
 from flask import jsonify
+from projectJson import register_json
 
 def register_projects(app):
-  register_meta(app, jsonify)
+  register_meta(app, jsonify())
+  register_json(app, jsonify()) 
