23c3f041d51adf78d01cb02e499a2f792f68d841
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-08-03 11:32:59 +0300

    Create metadata.py

---
+++ src/projects/metadata.py
@@ -0,0 +1,9 @@
+from .. import db
+
+def register_meta(app, jsonify):
+  @app.route('/projects/<int:id>')
+  def meta(id):
+    json = db.query(f"SELECT {id} FROM projects as project; SELECT project.metadata")
+    return jsonify(json)
+
+  @a
