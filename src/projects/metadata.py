00ede35d9b466fbaa35c2e7ee067d4c6050a9062
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-08-05 15:23:27 +0100

    Delete src directory

--- src/projects/metadata.py
+++
@@ -1,7 +0,0 @@
-from src import db 
-
-def register_meta(app, jsonify):
-    @app.route('/projects/<int:id>')
-    def meta(id):
-        result = db.query(f"SELECT metadata FROM projects WHERE id = {id}")
-        return jsonify(result)
