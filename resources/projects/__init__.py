fdd70cb5b4a5705b77f8d83071515842dca04bca
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-08-05 15:31:08 +0100

    Update __init__.py

--- resources/projects/__init__.py
+++ resources/projects/__init__.py
@@ -3,9 +3,11 @@ from flask import jsonify
 from .projectJson import register_json
 from .projectAssets import register_asset
 from .createProject import create_project
+from .saveProject import save_project_sb3
 
 def register_projects(app):
     register_meta(app, jsonify)
     register_json(app, jsonify)
     register_asset(app, jsonify)
     create_project(app)
+    save_project_sb3(app)
